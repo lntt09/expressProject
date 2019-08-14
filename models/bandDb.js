7e3e463ad7468dbfccb2e761fbf368eb0164e83f
Quincy Hicks <qdh6159@gmail.com>
2019-08-13 21:35:03 -0500

    update heroku

--- models/bandDb.js
+++ models/bandDb.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-
-
+const mongoURI = process.env.MONGODB_URI;
+const db = mongoose.connection;
 
 const bandSchema = new Schema({
     bandName: String,
